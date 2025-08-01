4285c4ca0a58e75f4016fc8e38f49b552351edbb
harshanacz <mathzticksyt@Gmail.com>
2025-08-02 01:37:51 +0530

    Add user model with schema definition for name, email, password, avatar, and date

---
+++ models/userModel.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    default:
+      "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
