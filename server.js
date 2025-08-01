4cce49bdd9b86005b61e17eddf78fb046687af22
harshanacz <mathzticksyt@Gmail.com>
2025-08-02 01:10:52 +0530

    Refactor server.js to organize route imports and usage

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 
+// Importing routes
+import profileRoutes from "./routes/api/profile.js";
+import authRoutes from "./routes/api/auth.js";
+import postRoutes from "./routes/api/posts.js";
+import usersRoutes from "./routes/api/users.js";
+
 
 dotenv.config();
 
@@ -19,6 +25,13 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Welcome to the Client Management System API!");
 });
+
+
+// Using routes
+app.use("/api/profile", profileRoutes);
+app.use("/api/users", usersRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
