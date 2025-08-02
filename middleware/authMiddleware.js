2c95998b6a914c93473702a30103cd5d8d1f5a54
harshanacz <mathzticksyt@Gmail.com>
2025-08-02 21:51:58 +0530

    Fix import path for User model in authMiddleware and apply middleware to auth route

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+import User from '../models/userModel.js';
 
 export const authMiddleware = async (req, res, next) => {
   const token = req.header('Authorization')?.replace('Bearer ', '');
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,12 @@
 import express from "express";
 const router = express.Router();
+import {authMiddleware} from "../../middleware/authMiddleware.js";
 
 // @route  GET /api/auth
 // @desc   Test route
 // @access Public
 
-router.get("/", (req, res) => {
+router.get("/",authMiddleware, (req, res) => {
   res.send("Auth route is working!");
 });
 
