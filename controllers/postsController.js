2c5e5feb3e5e29cbcf155a7b3c1614ff327c2241
harshanacz <mathzticksyt@Gmail.com>
2025-08-08 00:31:31 +0530

    Implement post retrieval, liking, commenting, and deletion features in PostsController; update routes accordingly

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -45,6 +45,23 @@ class PostsController {
     }
   }
 
+
+  async getAPostbyId (req, res) {
+    try {
+      const post = await Post.findById(req.params.id).populate("user", ["name", "avatar"]);
+      if (!post) {
+        return res.status(404).json({ msg: "Post not found" });
+      }
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      if (err.kind === "ObjectId") {
+        return res.status(404).json({ msg: "Post not found" });
+      }
+      res.status(500).send("Server error");
+    }
+  }
+
   // Update a post
   async updatePost(req, res) {
     const { title } = req.body;
@@ -89,6 +106,91 @@ class PostsController {
       res.status(500).send("Server error");
     }
   }
+
+  async addLike(req, res) {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (!post) {
+        return res.status(404).json({ msg: "Post not found" });
+      }
+
+      // Check if the post has already been liked by the user
+      if (post.likes.some(like => like.user.toString() === req.user.id)) {
+        post.likes = post.likes.filter(like => like.user.toString() !== req.user.id);
+        await post.save();
+
+      }
+
+      post.likes.unshift({ user: req.user.id });
+      await post.save();
+      res.json(post.likes);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+
+  //add comment
+    async addComment(req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { text } = req.body;
+    try {
+      const post = await Post.findById(req.params.id);
+      if (!post) {
+        return res.status(404).json({ msg: "Post not found" });
+      }
+
+      const user = await User.findById(req.user.id).select("-password");
+      if (!user) {
+        return res.status(404).json({ msg: "User not found" });
+      }
+
+
+      const newComment = {
+        text,
+        user: user._id,
+        createdAt: Date.now(),
+      };
+
+      post.comments.unshift(newComment);
+      await post.save();
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+
+  //delete comment
+  async deleteComment(req, res) {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (!post) {
+        return res.status(404).json({ msg: "Post not found" });
+      }
+
+      const comment = post.comments.find(comment => comment.id === req.params.commentId);
+      if (!comment) {
+        return res.status(404).json({ msg: "Comment not found" });
+      }
+
+      // Check if the user is the owner of the comment
+      if (comment.user.toString() !== req.user.id) {
+        return res.status(401).json({ msg: "User not authorized" });
+      }
+
+      post.comments = post.comments.filter(comment => comment.id !== req.params.commentId);
+      await post.save();
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+
 }
 
 export default new PostsController();
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -14,19 +14,38 @@ router.post("/", authMiddleware, postsController.createPost);
 // @access Public
 router.get("/", postsController.getPosts);
 
-// // @route  GET /api/posts/:id
-// // @desc   Get a post by ID
-// // @access Public
-// router.get("/:id", PostsController.getPostById);
+// @route  GET /api/posts/:id
+// @desc   Get a post by ID
+// @access Public
+router.get("/:id", postsController.getAPostbyId);
 
 // @route  PUT /api/posts/:id
 // @desc   Update a post by ID
 // @access Public
 router.put("/:id", authMiddleware, postsController.updatePost);
 
+
+
 // @route  DELETE /api/posts/:id
 // @desc   Delete a post by ID
-// @access Public
+// @access Private
 router.delete("/:id", authMiddleware, postsController.deletePost);
+
+
+
+// @route  POST /api/posts/like/:id
+// @desc   Like a post/unlike a post
+// @access Private
+router.put("/like/:id", authMiddleware, postsController.addLike);
+
+// @route  POST /api/posts/comment/:id
+// @desc   Add a comment to a post
+// @access Private
+router.post("/comment/:id", authMiddleware, postsController.addComment);
+
+// @route  DELETE /api/posts/comment/:id/:commentId
+// @desc   Delete a comment from a post
+// @access Private
+router.delete("/comment/:id/:commentId", authMiddleware, postsController.deleteComment);
 
 export default router;
